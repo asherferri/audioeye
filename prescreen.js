258ffbd73c1d1c863bc8f177ddee4fe6d437f531
Asher Ferri <asher@Ashers-MacBook-Pro.local>
2021-01-06 11:48:04 -0500

    initial commit

---
+++ prescreen.js
@@ -0,0 +1,40 @@
+// // question 6
+
+// var index = 'coin'
+
+// var obj = {
+//     a: {
+//         'animal': 'rooster',
+//         'coin': 'penny'
+//     },
+//     b: {
+//         'animal': 'hen',
+//         'coin': 'dime'
+//     }
+// }
+
+// console.log(obj.a.animal + " " + obj.b[index])
+
+//question 7
+
+var x = {
+    value: 0,
+    
+    plus: function () {
+        this.value = this.value + 1
+    },
+
+    minus: function () {
+        this.value = this.value - 1
+    },
+
+    display: function () {
+        console.log('value', this.value)
+    }
+}
+
+x.plus()
+x.plus()
+x.minus()
+x.plus()
+x.display()
